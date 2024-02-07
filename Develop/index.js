f7f80c81d43be38f7fd3bdf3905528e1249a081d
Landan Bichard <landanbichard@gmail.com>
2024-02-07 16:52:12 -0500

    Updating commits

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
-// TODO: Include packages needed for this application
+// Import external Node JS modules and js file.
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// List of questions to prompt user to answer.
+const questions = [
+    "Enter your project title",
+    "Enter a description",
+    "Enter installation instructions",
+    "Enter usage information",
+    "Enter credits",
+    "Choose a license for your application",
+    "Enter features of your project",
+    "Enter contribution guidelines",
+    "Enter test instructions",
+    "Enter your GitHub username",
+    "Enter your email address"
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile (data) {
+    fs.writeFile('sampleREADME.md', data, (err) =>
+        err ? console.error(err) : console.log('README file has been successfully created.'))
+};
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
